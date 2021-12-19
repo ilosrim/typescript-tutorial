ddc7ba579460318efe6ee0cffe414229b8c4aa7e
ilosrim <ilosrim@yahoo.com>
2021-12-19 22:16:24 +0500

    array, object

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,67 @@
-// Day 1
-var character = "mario";
-var age = 23;
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
-// character = 20;
-character = "luigi";
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// // Day 1
+// let character: string = "mario";
+// let age: number = 23;
+// console.log(character);
+// let inputs = document.querySelectorAll("input");
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+// // character = 20;
+// character = "luigi";
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(circ(23));
+// // =================== //
+// // Day 2
+// // array
+// let arr: string[] = ["Ali", "Vali", "G'ani"];
+// arr.push("Eshmat");
+// arr[1] = "Toshmat";
+// let arr2: any[] = ["Salom", 2022, "yil"];
+// arr2.push(2);
+// arr2[2] = 56;
+// // Objects
+// const myObj = {
+//   name: "vali",
+//   age: 23,
+// };
+// explict types
+var character = null;
+var age;
+var isLoggedIn;
+// arrays
+var tree = [];
+var lists;
+var listsTwo = [];
+listsTwo.push(2);
+listsTwo.push("hello");
+// tree = ["a", "b"];
+tree.push("c");
+// union types
+var mixed = [];
+mixed.push("blah");
+mixed.push(123);
+mixed.push(true);
+console.log(mixed);
+var uid;
+uid = "23";
+uid = 23;
+// objects
+var codOne;
+codOne = {
+    name: "Ilosrim",
+    age: 23,
+    status: "student"
 };
-console.log(circ(23));
-// =================== //
-// Day 2
-// array
-var arr = ["Ali", "Vali", "G'ani"];
-arr.push("Eshmat");
-arr[1] = "Toshmat";
-var arr2 = ["Salom", 2022, "yil"];
-arr2.push(2);
-arr2[2] = 56;
-// Objects
-var myObj = {
-    name: "vali",
-    age: 23
+var codeTwo;
+// codeTwo.name = "Jack";
+// codeTwo.age = 23;
+// codeTwo.isLoggedIn = false;
+codeTwo = {
+    name: "mario",
+    age: 23,
+    isLoggedIn: false,
+    skils: ["html", "css", "js"]
 };
+console.log(codeTwo);
--- sandbox.ts
+++ sandbox.ts
@@ -1,36 +1,91 @@
-// Day 1
-let character: string = "mario";
-let age: number = 23;
-console.log(character);
+// // Day 1
+// let character: string = "mario";
+// let age: number = 23;
+// console.log(character);
 
-let inputs = document.querySelectorAll("input");
-inputs.forEach((input) => {
-  console.log(input);
-});
+// let inputs = document.querySelectorAll("input");
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
 
-// character = 20;
-character = "luigi";
+// // character = 20;
+// character = "luigi";
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-};
-console.log(circ(23));
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(circ(23));
 
-// =================== //
-// Day 2
-// array
+// // =================== //
+// // Day 2
+// // array
 
-let arr: string[] = ["Ali", "Vali", "G'ani"];
-arr.push("Eshmat");
-arr[1] = "Toshmat";
+// let arr: string[] = ["Ali", "Vali", "G'ani"];
+// arr.push("Eshmat");
+// arr[1] = "Toshmat";
 
-let arr2: any[] = ["Salom", 2022, "yil"];
-arr2.push(2);
-arr2[2] = 56;
+// let arr2: any[] = ["Salom", 2022, "yil"];
+// arr2.push(2);
+// arr2[2] = 56;
 
-// Objects
+// // Objects
 
-const myObj = {
-  name: "vali",
+// const myObj = {
+//   name: "vali",
+//   age: 23,
+// };
+
+// explict types
+let character: string = null;
+let age: number;
+let isLoggedIn: boolean;
+
+// arrays
+let tree: string[] = [];
+let lists: number[];
+let listsTwo: any[] = [];
+listsTwo.push(2);
+listsTwo.push("hello");
+
+// tree = ["a", "b"];
+tree.push("c");
+
+// union types
+let mixed: (string | number | boolean)[] = [];
+mixed.push("blah");
+mixed.push(123);
+mixed.push(true);
+
+console.log(mixed);
+
+let uid: string | number;
+uid = "23";
+uid = 23;
+
+// objects
+let codOne: object;
+codOne = {
+  name: "Ilosrim",
   age: 23,
+  status: "student",
 };
+
+let codeTwo: {
+  name: string;
+  age: number;
+  isLoggedIn: boolean;
+  skils: string[];
+};
+
+// codeTwo.name = "Jack";
+// codeTwo.age = 23;
+// codeTwo.isLoggedIn = false;
+
+codeTwo = {
+  name: "mario",
+  age: 23,
+  isLoggedIn: false,
+  skils: ["html", "css", "js"],
+};
+
+console.log(codeTwo);
