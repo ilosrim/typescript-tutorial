e9b1467a1bb6dd8dd7de038bc0f83fb826cd9654
ilosrim <ilosrim@yahoo.com>
2021-12-22 00:42:48 +0500

    modules

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,20 +1,4 @@
-"use strict";
-class Invoice {
-    // readonly client: string; // o'zgartirish mumkin emas
-    // private details: string; // class-dan tashqarida ko'rinmaydi
-    // public amount: number; // ko'rinadi, o'zgartirsa bo'ladi
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-        // this.client = c;
-        // this.details = d;
-        // this.amount = a;
-    }
-    format() {
-        return `${this.client} ownes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("mario", "work on the mario website", 300);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices = [];
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,22 +1,4 @@
-class Invoice {
-  // readonly client: string; // o'zgartirish mumkin emas
-  // private details: string; // class-dan tashqarida ko'rinmaydi
-  // public amount: number; // ko'rinadi, o'zgartirsa bo'ladi
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {
-    // this.client = c;
-    // this.details = d;
-    // this.amount = a;
-  }
-
-  format() {
-    return `${this.client} ownes $${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 300);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
