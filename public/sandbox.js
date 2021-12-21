8d9aee3a1da11a418b1c42c0ed21fe082b8142f9
ilosrim <ilosrim@yahoo.com>
2021-12-22 00:14:00 +0500

    Classes

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,20 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} ownes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario website", 300);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,27 @@
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} ownes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("mario", "work on the mario website", 300);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 // inputs
